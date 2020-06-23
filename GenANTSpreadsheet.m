f62386d1ef98292a1bcd738cb69a8f37946f0dc5
Denis Fitzpatrick <denis.fitzpatrick@boystown.org>
2020-06-23 14:07:14 -0500

    Decreased number of trials

--- GenANTSpreadsheet.m
+++ GenANTSpreadsheet.m
@@ -5,7 +5,7 @@ ANTPracTable = ANTPracTable(...
     ANTPracTable.attop==1&ANTPracTable.spatial==0&ANTPracTable.nocue==1&ANTPracTable.both==0,:);
 %the above is an ad hoc solution to generating the desired number of
 %practice trials, which are always cued and in the middle
-numreps=10;
+numreps=5;
 ispractice=0;
 ANTTestTable = GenANTtable(numreps,ispractice);
 outtable = [ANTPracTable;ANTTestTable];
